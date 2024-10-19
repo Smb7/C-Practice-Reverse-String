7d8485779f65f18021aedfb398f5828df8c27571
Shane Brennan <shane.m.brenn@gmail.com>
2024-10-18 23:53:19 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.ExceptionServices;
+using System.Security.AccessControl;
+using System.Security.Cryptography.X509Certificates;
+
+class Program
+{   // Main is where the user will input and see the results
+    public static void Main(string[] args)
+    {
+        string? UserInput = Console.ReadLine();
+        Console.WriteLine($"You entered: {UserInput}");
+        string reversedInput = Reverse(UserInput);
+        Console.WriteLine($"Reverse string: {reversedInput}");
+        
+    }
+
+    // This method is the logic 
+    public static string Reverse(string Input)
+    {
+        // converts string to character array 
+        char[] charArray = Input.ToCharArray();
+
+        // declaring an empty string 
+        string reversedString = String.Empty;
+
+        // iterate through each character right to left
+        for (int i = charArray.Length - 1; i > -1; i--)
+        {
+            reversedString += charArray[i];
+        }
+        return reversedString;
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# C-Practice-Reverse-String
